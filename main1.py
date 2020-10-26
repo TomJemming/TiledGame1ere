40c47d10a6d3d4fa70ecb9734a0427b4e891d8f4
TomJemming <tom.jemming@yahoo.com>
2020-10-26 13:15:49 +0100

    alr map - better mouvement+camera roll
    - First map (silhouette alr)
    - new player mouvement
    - camera follows the player

--- main1.py
+++ main1.py
@@ -1,13 +1,14 @@
 import pygame as pg
 import random
-import sys
 import os
 
 HEIGHT = 1080
 WIDTH = 1920
-FPS = 10
+FPS = 60
 
-GRID_SIZE = 64
+SPEED = 16
+
+GRID_SIZE = 32
 GRID_HEIGHT = HEIGHT/GRID_SIZE
 GRID_WIDTH = WIDTH/GRID_SIZE
 
@@ -26,19 +27,45 @@ class Player(pg.sprite.Sprite):
         self.image.fill(GREEN)
         self.x = x
         self.y = y
+        self.mx = 0
+        self.my = 0
         self.rect = self.image.get_rect()
-
-    def movement(self, mx=0, my=0):
-        self.x += mx
-        if g.wall_collision():
-            self.x -= mx
-        self.y += my
-        if g.wall_collision():
-            self.y -= my
-
-    def update(self):
         self.rect.x = self.x * GRID_SIZE
         self.rect.y = self.y * GRID_SIZE
+        self.R = 0
+        self.G = 255
+        self.B = 0
+
+    def movement(self, mx=0, my=0):
+        if not self.wall_collision(mx=mx):
+            if self.rect.x == self.x * GRID_SIZE:
+                self.x += mx
+                self.mx = mx
+
+        if not self.wall_collision(my=my):
+            if self.rect.y == self.y * GRID_SIZE:
+                self.y += my
+                self.my = my
+
+    def wall_collision(self, mx=0, my=0):
+        player_cords = (self.x + mx, self.y + my)
+        for wall in g.all_walls:
+            if wall == player_cords:
+                return True
+        return False
+
+    def update(self):
+        if self.rect.x != self.x * GRID_SIZE:
+            self.rect.x += self.mx * SPEED
+            self.R = random.randrange(0,255)
+            self.G = random.randrange(0,255)
+            self.B = random.randrange(0,255)
+        if self.rect.y != self.y * GRID_SIZE:
+            self.rect.y += self.my * SPEED
+            self.R = random.randrange(0,255)
+            self.G = random.randrange(0,255)
+            self.B = random.randrange(0,255)
+        self.image.fill((self.R, self.G ,self.B))
 
 
 class Wall(pg.sprite.Sprite):
@@ -53,13 +80,29 @@ class Wall(pg.sprite.Sprite):
         self.rect.y = self.y * GRID_SIZE
 
 
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pg.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def get_offset(self, object):
+        return object.rect.move(self.camera.topleft)
+
+    def update(self, player):
+        x = -player.rect.x + int(WIDTH/2)
+        y = -player.rect.y + int(HEIGHT/2) -28
+
+        self.camera = pg.Rect(x, y, self.width, self.height)
+
+
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
         pg.display.set_caption("Allo.")
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(1, 200)
+        #pg.key.set_repeat(1, 200)
         self.running = True
 
     def load(self):
@@ -74,6 +117,7 @@ class Game:
         self.all_walls = []
         self.load()
         self.create_map()
+        self.camera = Camera(self.map_width, self.map_height)
         self.run()
 
     def run(self):
@@ -86,26 +130,24 @@ class Game:
     def grid(self):
         for i in range(0, WIDTH, GRID_SIZE):
             pg.draw.line(self.screen, DARKGREY, (i, 0), (i, HEIGHT))
+
         for i in range(0, HEIGHT, GRID_SIZE):
             pg.draw.line(self.screen, DARKGREY, (0, i), (WIDTH, i))
 
     def create_map(self):
+        self.map_width = len(self.map[0])
+        self.map_height = len(self.map)
+
         for posy, line in enumerate(self.map):
             for posx, symbol in enumerate(line):
                 if symbol == "W":
                     w = Wall(self, x=posx, y=posy)
                     self.all_sprites.add(w)
                     self.all_walls.append((posx, posy))
+
                 if symbol == "P":
                     self.player = Player(self, posx, posy)
                     self.all_sprites.add(self.player)
-
-    def wall_collision(self):
-        player_cords = (self.player.x, self.player.y)
-        for wall in self.all_walls:
-            if wall == player_cords:
-                return True
-        return False
 
     def events(self):
         for event in pg.event.get():
@@ -113,28 +155,36 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
+
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    self.player.movement(my=-1)
-                if event.key == pg.K_s:
-                    self.player.movement(my=+1)
-                if event.key == pg.K_a:
-                    self.player.movement(mx=-1)
-                if event.key == pg.K_d:
-                    self.player.movement(mx=+1)
                 if event.key == pg.K_ESCAPE:
                     if self.playing:
                         self.playing = False
                     self.running = False
 
+        keys_pressed = pg.key.get_pressed()
+
+        if keys_pressed[pg.K_LEFT] or keys_pressed[pg.K_a]:
+            self.player.movement(mx=-1)
+        if keys_pressed[pg.K_RIGHT] or keys_pressed[pg.K_d]:
+            self.player.movement(mx=1)
+        if keys_pressed[pg.K_UP] or keys_pressed[pg.K_w]:
+            self.player.movement(my=-1)
+        if keys_pressed[pg.K_DOWN] or keys_pressed[pg.K_s]:
+            self.player.movement(my=1)
+
     def draw(self):
         self.screen.fill(BLACK)
         self.grid()
-        self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.get_offset(sprite))
+
         pg.display.flip()
 
     def update(self):
         self.all_sprites.update()
+        self.camera.update(self.player)
+
         self.clock.tick(FPS)
 
 
